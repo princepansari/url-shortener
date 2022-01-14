b980693c71ebe0784eab79fc1b4a61a15d1e765a
Prince Pansari <princep@iitbhilai.ac.in>
2022-01-14 14:35:43 +0530

    get_original_url_dev API added

--- app/url_service/src/get_original_url.py
+++ app/url_service/src/get_original_url.py
@@ -1,4 +1,4 @@
-from flask import redirect
+from flask import request, redirect
 from flask_restful import Resource
 from http import HTTPStatus
 from app.common.rds import RDS
@@ -14,3 +14,15 @@ class GetOriginalUrl(Resource):
             # TODO - change the json output to the html page for invalid link
             return {'error': 'Invalid shortened link'}, HTTPStatus.BAD_REQUEST
         return redirect(original_link, code=302)
+
+
+class GetOriginalUrlDev(Resource):
+    def __init__(self):
+        self.rds = RDS()
+
+    def get(self):
+        shortened_link = request.args['shortened_link']
+        original_link = self.rds.get_original_link(shortened_link=shortened_link)
+        if original_link is None:
+            return {'error': 'Invalid shortened link'}, HTTPStatus.BAD_REQUEST
+        return {'original_link': original_link}, HTTPStatus.OK
