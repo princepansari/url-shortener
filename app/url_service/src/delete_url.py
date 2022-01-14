d7767b4a78e13ecb52b6cbedc454f0fb3e2dcac4
Prince Pansari <princep@iitbhilai.ac.in>
2022-01-14 15:30:56 +0530

    minor fixes

--- app/url_service/src/delete_url.py
+++ app/url_service/src/delete_url.py
@@ -15,7 +15,8 @@ class DeleteUrl(Resource):
         email = get_jwt_identity()
         data = request.get_json()
         shortened_link = Utils.remove_prefix(data['shortened_link'], Config.URL_ENDPOINT)
-
+        if shortened_link is None:
+            return {'error': 'Invalid shortened link'}, HTTPStatus.BAD_REQUEST
         user_id = self.rds.get_user_by_email(email=email)
         return UtilsDelete.delete_shortened_link(rds=self.rds,
                                                  user_id=user_id,
@@ -30,7 +31,8 @@ class DeleteUrlDev(Resource):
         data = request.get_json()
         developer_key = data['developer_key']
         shortened_link = Utils.remove_prefix(data['shortened_link'], Config.URL_ENDPOINT)
-
+        if shortened_link is None:
+            return {'error': 'Invalid shortened link'}, HTTPStatus.BAD_REQUEST
         user_id = self.rds.get_user_by_key(developer_key=developer_key)
         if user_id is None:
             return {'error': 'Invalid user'}, HTTPStatus.UNAUTHORIZED
