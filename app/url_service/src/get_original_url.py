de3bb570ce2ea5f94aba50b79496cbafcbe824ab
siramNIkhil2000 <siram.kumar@needl.ai>
2022-01-14 19:20:07 +0530

    * rebased with the latest master and did relevant changes

--- app/url_service/src/get_original_url.py
+++ app/url_service/src/get_original_url.py
@@ -13,6 +13,15 @@ class GetOriginalUrl(Resource):
         if original_link is None:
             # TODO - change the json output to the html page for invalid link
             return {'error': 'Invalid shortened link'}, HTTPStatus.BAD_REQUEST
+
+        is_malicious, malicious_class = GetOriginalUrlUtils.is_malicious_link(self.rds, original_link)
+
+        if is_malicious:
+            # TODO - change the json output to the html page for malicious link
+            return {'original_link': original_link,
+                    'is_malicious': is_malicious,
+                    'class_of_malicious_link': malicious_class}, HTTPStatus.OK
+
         return redirect(original_link, code=302)
 
 
@@ -25,14 +34,17 @@ class GetOriginalUrlDev(Resource):
         if original_link is None:
             return {'error': 'Invalid shortened link'}, HTTPStatus.BAD_REQUEST
 
-        is_malicious, malicious_class = self.is_malicious_link(original_link)
+        is_malicious, malicious_class = GetOriginalUrlUtils.is_malicious_link(self.rds, original_link)
 
         return {'original_link': original_link,
                 'is_malicious': is_malicious,
                 'class_of_malicious_link': malicious_class}, HTTPStatus.OK
 
 
-    def is_malicious_link(self, *, link):
-        link_detail = self.rds.get_link_detail(link=link)
+class GetOriginalUrlUtils:
+
+    @staticmethod
+    def is_malicious_link(self, *, rds, link):
+        link_detail = rds.get_link_detail(link=link)
         return (True, link_detail['class_of_malicious_link']) if link_detail else (False, None)
 
