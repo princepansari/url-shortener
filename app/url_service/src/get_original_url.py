c4cfd0aa0347c8cc004f0c80ee6cf54cec590039
Prince Pansari <princep@iitbhilai.ac.in>
2022-01-15 12:45:12 +0530

    logic for request check added for remaining url_service

--- app/url_service/src/get_original_url.py
+++ app/url_service/src/get_original_url.py
@@ -1,14 +1,20 @@
-from flask import request, redirect
+from flask import redirect
 from flask_restful import Resource
 from http import HTTPStatus
 from app.common.rds import RDS
+from schema import Schema, And, Use
+import bleach
 
 
 class GetOriginalUrl(Resource):
     def __init__(self):
         self.rds = RDS()
+        self.schema = Schema(And(str, Use(bleach.clean)))
 
     def get(self, shortened_link):
+        if not self.schema.is_valid(shortened_link):
+            return {'error': 'Invalid payload'}, HTTPStatus.BAD_REQUEST
+        shortened_link = self.schema.validate(shortened_link)
         original_link = self.rds.get_original_link(shortened_link=shortened_link)
         if original_link is None:
             # TODO - change the json output to the html page for invalid link
@@ -19,8 +25,12 @@ class GetOriginalUrl(Resource):
 class GetOriginalUrlDev(Resource):
     def __init__(self):
         self.rds = RDS()
+        self.schema = Schema(And(str, Use(bleach.clean)))
 
     def get(self, shortened_link):
+        if not self.schema.is_valid(shortened_link):
+            return {'error': 'Invalid payload'}, HTTPStatus.BAD_REQUEST
+        shortened_link = self.schema.validate(shortened_link)
         original_link = self.rds.get_original_link(shortened_link=shortened_link)
         if original_link is None:
             return {'error': 'Invalid shortened link'}, HTTPStatus.BAD_REQUEST
