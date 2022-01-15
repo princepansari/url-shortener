fbe12b6175ba124619758260f20fdffa4cd159db
siramNIkhil2000 <siram.kumar@needl.ai>
2022-01-15 19:49:32 +0530

    * added swagger support

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from .auth.routes import initialize_routes
 from app.url_service.routes import url_service_routes
+from app.swagger.routes import initialize_swagger_routes
 
 # Define the WSGI application object
 app = Flask(__name__)
@@ -18,3 +19,4 @@ jwt = JWTManager(app)
 
 initialize_routes(api)
 url_service_routes(api)
+initialize_swagger_routes(api)
---
+++ app/swagger/src/get_docs.py
@@ -0,0 +1,8 @@
+from flask_restful import Resource
+from flask import Response, render_template
+
+class GetDocs(Resource):
+
+    def get(self):
+        print('sending docs')
+        return Response(response=render_template('swaggerui.html'))
