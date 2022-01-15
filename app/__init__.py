db75d3983ac327b8d33c40ada7f394ad77ce6ec6
siramNIkhil2000 <siram.kumar@needl.ai>
2022-01-15 19:14:18 +0530

    * fixed CORS issue

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from .auth.routes import initialize_routes
 from app.url_service.routes import url_service_routes
+from app.swagger.routes import initialize_swagger_routes
+from flask_cors import CORS
 
 # Define the WSGI application object
 app = Flask(__name__)
@@ -13,8 +15,10 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 api = Api(app)
+cors = CORS(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
 initialize_routes(api)
 url_service_routes(api)
+initialize_swagger_routes(api)
