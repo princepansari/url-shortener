2e8033c9d17b1459d80c344bb173775e2ff3cdf9
Prince Pansari <princep@iitbhilai.ac.in>
2022-01-15 22:02:18 +0530

    taking data from args in delete

--- app/url_service/src/delete_url.py
+++ app/url_service/src/delete_url.py
@@ -3,8 +3,6 @@ from flask_restful import Resource
 from http import HTTPStatus
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.common.rds import RDS
-from app.common.config import Config
-from app.common.utilities import Utils
 from schema import Schema, And, Use
 import bleach
 
@@ -13,16 +11,16 @@ class DeleteUrl(Resource):
     def __init__(self):
         self.rds = RDS()
         self.schema = Schema({
-            'shortened_link': And(str, Use(bleach.clean), len),
+            'alias': And(str, Use(bleach.clean), len),
         })
 
     @jwt_required()
     def delete(self):
         email = get_jwt_identity()
-        if not self.schema.is_valid(request.get_json()):
+        if not self.schema.is_valid(request.args.to_dict()):
             return {'error': 'Invalid payload'}, HTTPStatus.BAD_REQUEST
-        data = self.schema.validate(request.get_json())
-        shortened_link = Utils.remove_prefix(data['shortened_link'], Config.READ_URL)
+        data = self.schema.validate(request.args.to_dict())
+        shortened_link = data['alias']
         if shortened_link is None:
             return {'error': 'Invalid shortened link'}, HTTPStatus.BAD_REQUEST
         user_id = self.rds.get_user_by_email(email=email)
@@ -36,15 +34,15 @@ class DeleteUrlDev(Resource):
         self.rds = RDS()
         self.schema = Schema({
             'developer_key': And(str, Use(bleach.clean), len),
-            'shortened_link': And(str, Use(bleach.clean), len),
+            'alias': And(str, Use(bleach.clean), len),
         })
 
     def delete(self):
-        if not self.schema.is_valid(request.get_json()):
+        if not self.schema.is_valid(request.args.to_dict()):
             return {'error': 'Invalid payload'}, HTTPStatus.BAD_REQUEST
-        data = self.schema.validate(request.get_json())
+        data = self.schema.validate(request.args.to_dict())
         developer_key = data['developer_key']
-        shortened_link = Utils.remove_prefix(data['shortened_link'], Config.READ_URL)
+        shortened_link = data['alias']
         if shortened_link is None:
             return {'error': 'Invalid shortened link'}, HTTPStatus.BAD_REQUEST
         user_id = self.rds.get_user_by_key(developer_key=developer_key)
